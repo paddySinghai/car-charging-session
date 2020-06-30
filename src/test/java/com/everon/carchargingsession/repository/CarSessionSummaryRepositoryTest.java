838f68df3b99cd0b965a71b830b586614808f654
Padmalaya <padmalaya.singhai@hotmail.com>
2020-06-30 20:05:37 +0200

    Removed compiletime error  in junit

--- src/test/java/com/everon/carchargingsession/repository/CarSessionSummaryRepositoryTest.java
+++ src/test/java/com/everon/carchargingsession/repository/CarSessionSummaryRepositoryTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;it
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
